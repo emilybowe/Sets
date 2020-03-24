ce49adf997f7d5fd2a2202ea378159d3e5e89533
Emily Bowe <ebowe@tcd.ie>
2020-03-24 18:27:30 +0000

    implement Product class

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,20 @@
 public class Product {
+    private int weight;
+    private String description;
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
